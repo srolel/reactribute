4a2556147738744426f4b6b90074fc1851692cfe
Sharon Rolel <sharonr@tipranks.com>
2015-10-27 22:51:28 +0200

    remove dbug

--- dbug.js
+++
@@ -1,3 +0,0 @@
-var ironNode = require('C:/Users/Sharon/AppData/Roaming/npm/node_modules/iron-node/bin/export.js');
-
-ironNode(['node_modules/babel/bin/babel-node.js', 'node_modules/jest-cli/src/jest.js']);
